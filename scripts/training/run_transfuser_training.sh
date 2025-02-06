b4d7314d0a3cb64a52784823ad208d8ef95814d3
hongxiaoy <yhx_ai@126.com>
2025-02-06 14:17:05 +0800

    Update force caching to false in script.

--- scripts/training/run_transfuser_training.sh
+++ scripts/training/run_transfuser_training.sh
@@ -4,3 +4,4 @@ python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
 agent=transfuser_agent \
 experiment_name=training_transfuser_agent \
 train_test_split=$TRAIN_TEST_SPLIT \
+force_cache_computation=false \
